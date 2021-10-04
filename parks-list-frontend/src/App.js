66804b81c58c2a25435146e8ccdfd36fd18f3bfe
jmalabed <jmalabed27@gmail.com>
2021-10-04 10:55:45 -0700

    updated react-bootstrap

--- parks-list-frontend/src/App.js
+++ parks-list-frontend/src/App.js
@@ -1,9 +1,23 @@
-
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </header>
+
       <h1>Ground Zero</h1>
     </div>
   );
