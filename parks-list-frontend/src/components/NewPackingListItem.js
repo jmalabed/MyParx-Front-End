17d2b6c2efd4b379508248403416bff562d2b3eb
Jess LaFrank <jesslafrank@gmail.com>
2021-10-04 19:03:09 -0700

    added comments for functions to code

--- parks-list-frontend/src/components/NewPackingListItem.js
+++ parks-list-frontend/src/components/NewPackingListItem.js
@@ -1,6 +1,17 @@
 import React from 'react'
 
 const NewPackingListItem = (props) => {
+
+  /* useState / useEffect ? */
+
+  /* function to find packingList id/name? to pass through via a hidden field */
+
+  /* function to handleChange to input field and update state */
+
+  /* function to create packingListItem */
+
+  /* function to handleSubmit */
+
   return (
     <div>
       <h2>New Packing List Item</h2>
@@ -8,7 +19,7 @@ const NewPackingListItem = (props) => {
       {/* form to add an item to the packing list */}
       <form>
         <label>Item</label>
-        <input type="hidden" value="**TOFIX**packinglist"/>
+        <input type="hidden" value="TOFIX"/>
         <input type="text"/>
         {/*this should auto grab id of packing list from parent component */}
         <input type="submit" />
