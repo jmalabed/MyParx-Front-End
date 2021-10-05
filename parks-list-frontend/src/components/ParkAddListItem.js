0f405ae6fcaf6bf5c29417205cc3c041ec0a967e
jmalabed <jmalabed27@gmail.com>
2021-10-05 08:37:32 -0700

    updates to add park and search

--- parks-list-frontend/src/components/ParkAddListItem.js
+++ parks-list-frontend/src/components/ParkAddListItem.js
@@ -1,7 +1,25 @@
 const ParkAddListItem = (props) => {
+  const handleSubmit = async (e) => {
+    await fetch("http://localhost:9000/bucketList", {
+      body: JSON.stringify(),
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
+
   return (
-    <li>
-      <p>This has data for now</p>
-    </li>
+    <tr>
+      <td>{props.parkprop.name}</td>
+      <td>{props.parkprop.states}</td>
+      <td>{props.parkprop.description.split(".")[0]}.</td>
+      <td>Add Favorite</td>
+      <form onSubmit={handleSubmit}>
+        <input type="hidden" value={parks.parkprop.name} name="name" />
+        <input type="hidden" value={parks.parkprop.states} name="states" />
+      </form>
+    </tr>
   );
 };
+export default ParkAddListItem;
