0ab70a9f3665782449b5fd79b0614d7d55858cd4
jmalabed <jmalabed27@gmail.com>
2021-10-06 22:50:14 -0700

    update packing list details

--- parks-list-frontend/src/components/PackingListDetails.js
+++ parks-list-frontend/src/components/PackingListDetails.js
@@ -105,7 +105,9 @@ const PackingListDetails = (props) => {
       .map((item) => (
         <tr key={item._id}>
           <td>{item.item}</td>
-          <td onClick={() => editItem(item)}>{item.isPacked ? "Yes" : "No"}</td>
+          <td onClick={() => editItem(item)}>
+            {item.isPacked ? <button>Yes</button> : <button>No</button>}
+          </td>
           <td>
             <button onClick={() => deleteItem(item._id)}>X</button>
           </td>
