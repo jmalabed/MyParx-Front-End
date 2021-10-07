4711e2d6cc7b60159c1751b414b8796773244c00
jmalabed <jmalabed27@gmail.com>
2021-10-06 23:34:28 -0700

    editing edit form and table

--- parks-list-frontend/src/components/ParkList.js
+++ parks-list-frontend/src/components/ParkList.js
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react";
-import { Table, Container, } from "react-bootstrap";
-
-console.log('import function', packingListFunc);
+import { Table, Container } from "react-bootstrap";
 
 const ParkList = (props) => {
   const [parks, setParks] = useState([]);
 
   const getParks = async () => {
     try {
-      const parks = await fetch("https://project-two-backend.herokuapp.com/bucketList");
+      const parks = await fetch(
+        "https://project-two-backend.herokuapp.com/bucketList"
+      );
       const parsedParks = await parks.json();
       console.log(parsedParks);
       setParks(parsedParks);
@@ -18,14 +18,17 @@ const ParkList = (props) => {
   };
 
   const editPark = async (id) => {
-    const updatedPark = await fetch(`https://project-two-backend.herokuapp.com/bucketList/${id}`, {
-      method: "PUT",
-      body: JSON.stringify({}),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const parsedPark = await updatedPark.json()
+    const updatedPark = await fetch(
+      `https://project-two-backend.herokuapp.com/bucketList/${id}`,
+      {
+        method: "PUT",
+        body: JSON.stringify({}),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const parsedPark = await updatedPark.json();
     console.log(parsedPark);
   };
 
@@ -49,10 +52,17 @@ const ParkList = (props) => {
   const parkRow = parks.map((park) => (
     <tr>
       <td>{park.name}</td>
+      <td>packingList</td>
       <td>{park.visited ? "Yes" : "No"}</td>
       <td>Pets</td>
-      <td>{park.people}</td>
-      <td>Date of Visit</td>
+      <td>
+        <ul>
+          {park.people.map((person) => (
+            <li>{person}</li>
+          ))}
+        </ul>
+      </td>
+      <td>{park.date}</td>
       <td onClick={() => editPark(park._id)}>
         <button>
           <a href={"/parklist/" + park._id + "/edit"}>Edit</a>
@@ -64,9 +74,9 @@ const ParkList = (props) => {
     </tr>
   ));
 
-  const checkBox = parks.map(park => {
+  const checkBox = parks.map((park) => {
     console.log(parks);
-  })
+  });
 
   useEffect(() => {
     getParks();
@@ -80,7 +90,6 @@ const ParkList = (props) => {
           <thead>
             <tr>
               <th>Park Name</th>
-              <th>State</th>
               <th>PackingList</th>
               <th>Visited</th>
               <th>Pets</th>
